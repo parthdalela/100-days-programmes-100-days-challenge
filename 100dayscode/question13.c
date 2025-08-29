27fecfc3f41b4ef23ea449e4484c27dbeaef77c4
parthdalela <parthdalela@gmail.com>
2025-08-29 09:12:27 +0530

    Add files via upload

---
+++ 100dayscode/question13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("enter a year = ");
+    scanf("%d", &year);
+    if (year % 4 == 0)
+    {
+        if(year%100!=0)
+        {
+            printf("year entered is a leap year");
+        }
+    }
+    if(year%400==0)
+    {
+        printf("entered year is a leap year");
+    }
+    else{
+          printf("year entered is not a leap year");
+    }
+    return 0;
+}
